7532425ea627ad9f3b229d886477ec5de45f988e
Dent-Harvey <126341167+Dent-Harvey@users.noreply.github.com>
2025-09-15 11:04:24 +0200

    Fix Metro native module import error for web

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Add resolver for native-only modules to prevent web build errors
+config.resolver.extraNodeModules = {
+  'react-native/Libraries/Utilities/codegenNativeCommands': path.resolve(__dirname, 'metro-mocks/codegenNativeCommands.js'),
+};
 
 // Add support for additional file extensions
 config.resolver.assetExts.push(
